8e11988cb0802d10b266f64d52416dd31288715a
Nic Bonetto <nick.bonetto@gmail.com>
2023-02-12 18:09:33 -0800

    feat: create basic landing page

---
+++ src/www/components/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Typewriter from './typewriter';
+
+function App() {
+  return (
+    <Typewriter/>
+  )
+}
+
+export default App
